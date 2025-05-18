b714ce414fda121d271da5624d822f9ad87dfe87
helenatheise <helena.theise@gmail.com>
2025-05-18 11:05:11 +0200

    add sastoken logic

--- src/functions/getArtImages.ts
+++ src/functions/getArtImages.ts
@@ -1,17 +1,33 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import { BlobServiceClient } from "@azure/storage-blob";
+import { BlobServiceClient, StorageSharedKeyCredential, BlobSASPermissions, SASProtocol, generateBlobSASQueryParameters} from "@azure/storage-blob";
 
-const connectionString = process.env["AzureWebJobsStorage"];
-const containerName = process.env["Storage_Container"];
+const containerName = process.env["STORAGE_CONTAINER"];
+const accountName = process.env["AZURE_STORAGE_ACCOUNT_NAME"];
+const accountKey = process.env["AZURE_STORAGE_ACCOUNT_KEY"];
 
 export async function getArtImages(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     try {
-        const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+        const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
+
+        const blobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net`, sharedKeyCredential);
+
         const containerClient = blobServiceClient.getContainerClient(containerName);
     
         const imageUrls: string[] = [];
+
         for await (const blob of containerClient.listBlobsFlat()) {
-          const blobUrl = `${containerClient.url}/${blob.name}`;
+          const sasToken = generateBlobSASQueryParameters(
+            {
+                containerName,
+                blobName: blob.name,
+                permissions: BlobSASPermissions.parse('r'),
+                expiresOn: new Date(new Date().valueOf() + 15 * 60 * 1000),
+                protocol: SASProtocol.HttpsAndHttp
+            }, 
+            sharedKeyCredential
+          ).toString();
+
+          const blobUrl = `${containerClient.url}/${blob.name}?${sasToken}`;
           imageUrls.push(blobUrl);
         }
     
